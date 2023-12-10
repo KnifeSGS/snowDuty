a5e68c7487e44d1bee947466398cec3874de7202
KnifeSGS <knife.sgs@gmail.com>
2023-12-11 00:19:44 +0100

    primeng 17 bugfix

--- frontend/duty/src/app/app.component.ts
+++ frontend/duty/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { FilterMatchMode, PrimeNGConfig } from 'primeng/api';
+import { LayoutService } from '../app/layout/services/layout.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,23 @@ export class AppComponent {
   title = 'duty';
 
   constructor(
-    private primengConfig: PrimeNGConfig
+    private primengConfig: PrimeNGConfig,
+    private layoutService: LayoutService
   ) { }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
+    this.primengConfig.zIndex = {
+      modal: 1100,    // dialog, sidebar
+      overlay: 1000,  // dropdown, overlaypanel
+      menu: 1000,     // overlay menus
+      tooltip: 1100   // tooltip
+    };
+    this.primengConfig.filterMatchModeOptions = {
+      text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
+      numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
+      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
+    };
     this.primengConfig.setTranslation({
       "startsWith": "Starts with",
       "contains": "Contains",
@@ -71,5 +84,13 @@ export class AppComponent {
       "emptyMessage": "Nem találtunk eredményt",
       "emptyFilterMessage": "Nincs a szűrésnek megfelelő eredmény"
     });
+    this.layoutService.config = {
+      ripple: true,                      //toggles ripple on and off
+      inputStyle: 'outlined',             //default style for input elements
+      menuMode: 'static',                 //layout mode of the menu, valid values are "static" and "overlay"
+      colorScheme: 'light',               //color scheme of the template, valid values are "light" and "dark"
+      theme: 'lara-light-teal',         //default component theme for PrimeNG
+      scale: 14                           //size of the body font size to scale the whole application
+    };
   }
 }
